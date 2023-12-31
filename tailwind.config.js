6d25d93e8c21c22bb8be7490a17f971f83da2fad
pouya <pouya.May4@gmail.com>
2024-01-01 02:03:59 +0330

    the header end

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontSize:{
+      norm: '22px',
+      tlg: '26px',
+      title: '110px',
+      usm: '16px',
+    },
+    
+  },
+  plugins: [],
+}
+
